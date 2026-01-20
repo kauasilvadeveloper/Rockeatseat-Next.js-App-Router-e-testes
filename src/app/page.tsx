5560fdddd1a6a863743b4f6a1fee3e113e4f13a5
Kauã Silva <kakaaandradesilva2008@gmail.com>
2026-01-20 18:15:05 -0300

    study: Next.js server components and Suspense API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
+import { Suspense } from "react";
 
-export default function Home() {
-  return <h1 className="">Olá mundo</h1>;
+import { GithubProfile } from "./components/github-profile";
+import { LongWaitComponent } from "./components/long-wait-component";
+
+export default async function Home() {
+  return (
+    <div>
+      <h1>home</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem numquam
+        blanditiis debitis illum consequuntur beatae! Quibusdam maxime molestias
+        nostrum! Sunt ducimus dicta temporibus, nemo officia fuga doloribus
+        quisquam laboriosam vel!
+      </p>
+      <Suspense fallback={<p>carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+
+      <Suspense fallback={<p>carregando GitHub profile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  );
 }
